180d40fba85c47bc30848d8f1a1b20423a7e4dd5
chrollos <106138109+chro11os@users.noreply.github.com>
2025-02-13 18:31:39 +0800

    added designs as a test

--- pfp/index.php
+++ pfp/index.php
@@ -4,15 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Neil Brags Guzman</title>
-</head>
-<body>
+    <!-- Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Javascript -->
+    <script src="script.js"></script>
 
+</head>
+<body>
     <div class="view">
-        <h1>Pogi mo eric</h1>
+        <h1>Neil</h1>
     </div>
-
 </body>
 </html>
